4acf4b661214155caf95ead3b9396f7bd66fec8a
Lars Hudson Lunde <laffen47@yahoo.no>
2024-05-20 23:17:36 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,11 @@ FILE=/firstrun
 if test -f "$FILE"; then
     rm -f $FILE
     echo "Docker NodeJS Testpages has been installed"
+    # Fix SSH Daemon
     mkdir /run/sshd
     chmod 750 /run/sshd
+    # Adding root login over ssh
+    echo "PermitRootLogin yes" > /etc/ssh/sshd_config.d/qdevice.conf
     echo "Start the docker the normal way and it will start normally"
     exit 0
 fi
