d2c81847262b7022ee7038c11120a3b65e24c766
Lars Hudson Lunde <laffen47@yahoo.no>
2024-05-20 14:58:26 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE=/firstrun
+if test -f "$FILE"; then
+    rm -f $FILE
+    echo "Docker NodeJS Testpages has been installed"
+    echo "Start the docker the normal way and it will start normally"
+    exit 0
+fi
+
+cd /application 
+node app.js
