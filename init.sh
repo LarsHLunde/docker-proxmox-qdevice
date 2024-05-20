9633b8650026ddd998c9914f8b8ab4c5f6cbd6e1
Lars Hudson Lunde <laffen47@yahoo.no>
2024-05-20 19:23:35 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ FILE=/firstrun
 if test -f "$FILE"; then
     rm -f $FILE
     echo "Docker NodeJS Testpages has been installed"
+    mkdir /run/sshd
+    chmod 750 /run/sshd
     echo "Start the docker the normal way and it will start normally"
     exit 0
 fi
