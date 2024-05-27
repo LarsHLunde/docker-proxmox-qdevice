364973f011179c9b9c0b6178635750390379782d
Lars Hudson Lunde <laffen47@yahoo.no>
2024-05-27 20:47:25 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,12 +3,13 @@
 FILE=/firstrun
 if test -f "$FILE"; then
     rm -f $FILE
-    echo "Docker NodeJS Testpages has been installed"
     # Fix SSH Daemon
     mkdir /run/sshd
     chmod 750 /run/sshd
     # Adding root login over ssh
     echo "PermitRootLogin yes" > /etc/ssh/sshd_config.d/qdevice.conf
+    echo "Starting device installer script ..."
+    lua setup.lua
     echo "Start the docker the normal way and it will start normally"
     exit 0
 fi
